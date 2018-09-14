3ae2238afc75b0957f69a6ee14c4d80b5e68f3c0
jdiperi88 <jdiperi88@yahoo.com>
2018-09-13 22:17:36 -0400

    created bar chart constructor and added a data object

---
+++ bar.js
@@ -0,0 +1,7 @@
+//bar.js
+//bar chart library
+//09/13/18
+
+function BarChart(targetId, width, height, data) {
+	console.log(arguments);
+}
--- canvas-script.js
+++ canvas-script.js
@@ -1,5 +1,30 @@
 window.onload = () => {
-	//definitions
-	var canvas = document.getElementById("canvas");
-	var context = canvas.getContext("2d");
+	var min = 1;
+	var max = 200;
+
+	// chart data
+	var data = [
+		{ label: "Jan", value: getRandomInt(min, max) },
+		{ label: "Feb", value: getRandomInt(min, max) },
+		{ label: "March", value: getRandomInt(min, max) },
+		{ label: "April", value: getRandomInt(min, max) },
+		{ label: "May", value: getRandomInt(min, max) },
+		{ label: "June", value: getRandomInt(min, max) },
+		{ label: "July", value: getRandomInt(min, max) },
+		{ label: "August", value: getRandomInt(min, max) },
+		{ label: "September", value: getRandomInt(min, max) }
+	];
+
+	//char specs
+	var targetId = "chart";
+	var canvasWidth = 600;
+	var canvasHeight = 450;
+
+	//create chart
+	var chart = new BarChart(targetId, canvasWidth, canvasHeight, data);
+	function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+	}
 };
