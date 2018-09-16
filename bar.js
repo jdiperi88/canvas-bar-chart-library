6ab2ccc65bbb44e3d3104daadad7b52197429b8d
jdiperi88 <jdiperi88@yahoo.com>
2018-09-16 11:35:03 -0400

    finished bar chart spec config in constructor

--- bar.js
+++ bar.js
@@ -5,7 +5,13 @@
 function BarChart(targetId, width, height, data) {
 	//base
 	var chart = this;
+	//specify config
 	chart.configureChart(targetId, width, height, data);
+
+	//pre operations
+	chart.performPreOperations();
+
+	console.log(chart);
 }
 
 BarChart.prototype.configureChart = function(targetId, width, height, data) {
@@ -54,4 +60,68 @@ BarChart.prototype.setChartParameters = function() {
 	//Guidelines config
 	chart.guidelineColor = "#e5e5e5";
 	chart.guidelineWidth = 0.5;
+};
+
+BarChart.prototype.performPreOperations = function() {
+	var chart = this;
+
+	// create canvas
+	chart.createCanvas();
+
+	//get data
+	chart.handleData();
+
+	// prepare data
+	chart.prepareData();
+};
+
+BarChart.prototype.createCanvas = function() {
+	//base
+	var chart = this;
+
+	var canvas = document.createElement("canvas");
+	canvas.id = chart.id + "-" + Math.random();
+	canvas.width = chart.width;
+	canvas.height = chart.ehgith;
+
+	// append canvas to target container
+	document.getElementById(chart.id).innerHTML = ""; // sets a clean container
+	document.getElementById(chart.id).appendChild(canvas); // add canvas to clean container
+
+	//add canvas to chart object
+	chart.canvas = canvas;
+	chart.context = canvas.getContext("2d");
+};
+
+BarChart.prototype.handleData = function() {
+	//base
+	var chart = this;
+
+	//data sets
+	chart.labels = [];
+	chart.values = [];
+
+	//handle data
+	chart.data.forEach(function(item) {
+		chart.labels.push(item.label);
+		chart.values.push(item.value);
+	});
+};
+
+BarChart.prototype.prepareData = function() {
+	var chart = this;
+
+	// global var
+	chart.itemNum = chart.data.length;
+	chart.maxValue = Math.max.apply(null, chart.values);
+	chart.minValue = Math.min.apply(null, chart.values);
+
+	// axe specs
+	chart.verticalAxeWidth - chart.height - 2 * chart.verticalMargin; // bottom and top margins
+	chart.horizontalAxeWidth - chart.width - 2 * chart.horizontalMargin; // left and right
+
+	//label specs
+	chart.verticalUpperBound = Math.ceil(chart.max / 10) * 10;
+	chart.verticalLabelFreq = chart.verticalUpperBound / chart.itemsNum;
+	chart.horizontalLabelFreq = chart.horizontalAxeWidth / chart.itemsNum;
 };
